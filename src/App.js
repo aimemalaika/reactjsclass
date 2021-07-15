baf5055572ed6245bf22cf0e79b5932f7538f358
Aime Malaika <aimemalaika1995@gmail.com>
2021-07-15 19:43:19 +0200

    added functions

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 
 import Header from './Header'
 import Intro from './Intro'
+
 class App extends Component {
+
+  sayHello = (name) => {
+    return "Hello " + name
+  }
+
+  constructor(props) {
+    super(props);
+    this.name = "malaika"
+    this.state = {}
+  }
+
   render(){
+    const name = "Aime"
     return (
       <div className="App">
-        <Header />
-        <Intro />
+        <h2>Just some sample data : { this.name }</h2>
       </div>
     );
   }
