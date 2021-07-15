1d2b7505ecb476a0c93b66643c719ba33e33cdcf
Aime Malaika <aimemalaika1995@gmail.com>
2021-07-15 19:09:51 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -2,21 +2,7 @@ import React,{ Component } from 'react'
 import './App.css';
 
 import Header from './Header'
-
-class Intro extends Component{
-  render(){
-    return(
-      <div>
-        <p>Basic object of a changed function to class</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </div>
-    )
-  }
-}
-
-
+import Intro from './Intro'
 class App extends Component {
   render(){
     return (
