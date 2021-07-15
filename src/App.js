064bd841ad3442bc8bb2b714a0bcab3b01542de5
Aime Malaika <aimemalaika1995@gmail.com>
2021-07-15 18:50:22 +0200

    creation of component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,37 @@ import React,{ Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+class Header extends Component {
+  render(){
+    return(
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    )
+  }
+}
+
+
+class Intro extends Component{
+  render(){
+    return(
+      <div>
+        <p>Basic object of a changed function to class</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </div>
+    )
+  }
+}
+
+
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Intro />
       </div>
     );
   }
