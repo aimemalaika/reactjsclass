778b9e0d869a054537f7cce089df860df1ce0d56
Aime Malaika <aimemalaika1995@gmail.com>
2021-07-19 09:18:41 +0200

    props in react

--- src/App.js
+++ src/App.js
@@ -2,27 +2,44 @@ import React,{ Component } from 'react'
 import './App.css';
 
 import Header from './Header'
-import Intro from './Intro'
 
 class App extends Component {
-
-  sayHello = (name) => {
-    return "Hello " + name
-  }
-
-  constructor(props) {
-    super(props);
-    this.name = "malaika"
-    this.state = {}
-  }
-
   render(){
-    const name = "Aime"
     return (
       <div className="App">
-        <h2>Just some sample data : { this.name }</h2>
+        <Header />
+        <Parent/>
       </div>
     );
+  }
+}
+
+class Parent extends Component {
+  render(){
+    return(
+      <div>
+        <h2>Just some info</h2>
+        <Cars  message="Cars are cool" model="Tesla Model X" coolcars={this.props.cars}/>
+      </div>
+    )
+  }
+}
+
+Parent.defaultProps = {
+  cars: ["Tesla", "Mazda", "Toyota", "BMW"]
+}
+
+class Cars extends Component {
+  render() {
+    console.log(this.props);
+    return(
+      <div>
+        <h3>I am from car component</h3>
+        <p>{this.props.message}</p>
+        <p>{this.props.model}</p>
+        <p>{this.props.coolcars.join()}</p>
+      </div>
+    )
   }
 }
 
